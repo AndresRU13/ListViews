145e713769d4478b1f75d976716c233df13cdd12
AndresRU13 <101481203+AndresRU13@users.noreply.github.com>
2022-10-06 12:43:22 -0500

    List and Adapter

---
+++ app/src/main/java/com/unipiloto/listviews/Activity_Drink.java
@@ -0,0 +1,14 @@
+package com.unipiloto.listviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Drink extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+    }
+}
---
+++ app/src/main/java/com/unipiloto/listviews/Drink.java
@@ -0,0 +1,46 @@
+package com.unipiloto.listviews;
+
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResource;
+
+    public static final Drink[] dirnks = {
+            new Drink("Latte", "A couple of espresso shots with steamed milk",
+                    R.drawable.latte),
+            new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam",
+                    R.drawable.cappuccino),
+            new Drink("Filter", "Highest quality beans roasted and brewed fresh",
+                    R.drawable.filter)
+    };
+
+    public Drink(String name, String description, int imageResource) {
+        this.name = name;
+        this.description = description;
+        this.imageResource = imageResource;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public void setImageResource(int imageResource) {
+        this.imageResource = imageResource;
+    }
+}
